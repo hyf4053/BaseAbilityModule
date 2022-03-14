4e77add5471ce4406cd667a9efb59fe9a70867c7
Lanmaomi <hyf4053@hotmail.com>
2022-03-14 16:23:26 +0800

    Initial Commit

---
+++ Source/BaseAbilityModule/BaseAbilityModuleGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BaseAbilityModuleGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BASEABILITYMODULE_API ABaseAbilityModuleGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
